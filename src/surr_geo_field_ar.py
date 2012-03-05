- fixed bug in construct_surrogates() with incorrect A*u multiplication - created script to estimate eigenvalues of covariance matrices

src/surr_geo_field_ar.py
@@ -8,6 +8,8 @@ Created on Thu Mar  1 17:02:39 2012
 
 from geo_field import GeoField
 from var_model import VARModel
+
+import cPickle
 import numpy as np
 
 
@@ -26,9 +28,9 @@ def _generate_surrogate(a):
     ndx = np.argsort(np.random.uniform(size = (len(res),)))
     r = np.zeros_like(res)
     r[ndx] = res
-    
+
     # run the simulation
-    return (i, j, v.simulate_with_residuals(r[:N, np.newaxis]))
+    return (i, j, v.simulate_with_residuals(r[:N, :]))
 
 
 class SurrGeoFieldAR(GeoField):
@@ -39,6 +41,31 @@ class SurrGeoFieldAR(GeoField):
     def __init__(self):
         """"""
         GeoField.__init__(self)
+        self.sd = None
+        
+        
+    def save_field(self, fname):
+        """Save the current field in a pickle file.
+           The current surrogate data is not saved and must be generated anew after unpickling."""
+        with open(fname, "w") as f:
+            cPickle.dump([self.d, self.lons, self.lats, self.tm, self.job_data, self.max_ord], f)
+
+        
+    def load_field(self, fname):
+        with open(fname, "r") as f:
+            lst = cPickle.load(f)
+            
+        self.d = lst[0]
+        self.lons = lst[1]
+        self.lats = lst[2]
+        self.tm = lst[3]
+        self.job_data = lst[4]
+        self.max_ord = lst[5]
+
+
+    def surr_data(self):
+        """Return the (hopefully already constructed) surrogate data."""
+        return self.sd
         
 
     def copy_field(self, other):
@@ -65,29 +92,31 @@ class SurrGeoFieldAR(GeoField):
         # burst the time series
         num_lats = len(self.lats)
         num_lons = len(self.lons)
-        
-        # model_grid and residuals are both dicts
-        self.residuals = {}
-        self.model_grid = {}
 
+        # identify each AR(k) model and compute the residuals
         job_data = [ (i, j, self.d[:, i, j]) 
                      for i in range(num_lats) for j in range(num_lons) ]
         
         job_results = map_func(_prepare_surrogates, job_data)
         
-        # store the job results in the object
-        max_ord = 0
+        # find out maximal order which bounds the length of generated surrogates
+        max_ord = max([r[2].order() for r in job_results])
+        num_tm_s = self.d.shape[0] - max_ord
+        
+        # create the job_data structure for the surrogate constructor function
+        job_data = []
         for i, j, v, r in job_results:
-            self.model_grid[(i,j)] = v
-            max_ord = max(max_ord, v.order())
-            self.residuals[(i,j)] = r[:, 0]
+            job_data.append((i, j, v, r, num_tm_s))
 
+        # store both items
         self.max_ord = max_ord
+        self.job_data = job_data
+        
         return True
     
 
     def construct_surrogate(self, pool = None):
-        """Construct a surrogate in-place."""
+        """Construct a new surrogate time series."""
         
         # optional parallel computation
         if pool == None:
@@ -97,16 +126,9 @@ class SurrGeoFieldAR(GeoField):
         
         num_lats = len(self.lats)
         num_lons = len(self.lons)
-        num_tm = len(self.tm)
-        
-        num_tm_s = num_tm - self.max_ord
+        num_tm_s = len(self.tm) - self.max_ord
         
         self.sd = np.zeros((num_tm_s, num_lats, num_lons))
-        
-        if self.job_data == None:
-            self.job_data = [ (i, j, self.model_grid[(i,j)], self.residuals[(i,j)], num_tm_s)
-                              for i in range(num_lats) for j in range(num_lons)]
-        
         sts_list = map_func(_generate_surrogate, self.job_data)
 
         for i, j, ts in sts_list: