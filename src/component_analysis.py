- incorrect indexing in sign flip in matched_components() fixed

src/component_analysis.py
@@ -152,7 +152,7 @@ def matched_components(U1, U2):
     # find the matching (even if there are more components in U)
     perm, sf = match_components_munkres(U1, U2)
     U2 = U2[:, perm[:C]]
-    U2 *= sf[:C]
+    U2 *= sf[:, :C]
     
     U2 /= np.sum(U2**2, axis = 0) ** 0.5
     return U2