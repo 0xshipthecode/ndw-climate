1245c21df15d2b656e1841a68cdeb4be10de973a
Martin Vejmelka <vejmelkam@gmail.com>
2012-04-10 23:34:14 +0200

    - added make figure script

--- scripts/render_figures_seminarustav2012.py
+++ scripts/render_figures_seminarustav2012.py
@@ -89,9 +89,67 @@ def render_model_order_slp():
         res = cPickle.load(f)
 
     
+
+def render_slp_nh_component_eigvals():
+    
+    with open('results/slp_eigvals_multi_surrogates_nh_var.bin', 'r') as f:
+        res = cPickle.load(f)
+        
+    dlam = res['dlam']
+    slam_ar = res['slam_ar']
+    slam_w1 = res['slam_w1']
+    slam_w2 = res['slam_w2']
+    slam_f = res['slam_f']
+    
+    slam_ar.sort(axis = 0)
+    slam_w1.sort(axis = 0)
+    slam_w2.sort(axis = 0)
+    slam_f.sort(axis = 0)
+    
+    N = len(dlam)
+    S = len(slam_ar)
+    p0025 = int(S * 0.025)
+    p0975 = int(S * 0.975)
+    p0500 = int(S * 0.5)
+    
+    plt.figure(figsize = (10,8))
+    
+    plt.title('Synthetic model eigenvalues from data and surrogates')    
+    plt.plot(np.arange(N) + 1, dlam, 'ro-', linewidth = 1.5)
+    mn = np.mean(slam_ar, axis = 0)
+    plt.errorbar(np.arange(N) + 1, mn, np.vstack([mn - slam_ar[p0025, :], slam_ar[p0975, :] - mn]), fmt = 'bo-')
+
+    mn = np.mean(slam_w1, axis = 0)
+    plt.errorbar(np.arange(N) + 1 + 0.1, mn, np.vstack([mn - slam_w1[p0025, :], slam_w1[p0975, :] - mn]), fmt = 'go-')
+    
+    mn = np.mean(slam_w2, axis = 0)
+    plt.errorbar(np.arange(N) + 1 + 0.2, mn, np.vstack([mn - slam_w2[p0025, :], slam_w2[p0975, :] - mn]), fmt = 'ko-')
+
+    mn = np.mean(slam_f, axis = 0)
+    plt.errorbar(np.arange(N) + 1 + 0.3, mn, np.vstack([mn - slam_f[p0025, :], slam_f[p0975, :] - mn]), fmt = 'mo-')
+    
+    plt.axes([0.3, 0.5, 0.5, 0.3], axisbg = 'w')
+    plt.axis([30, 50, 5, 30])
+    
+    plt.plot(np.arange(N) + 1, dlam, 'ro-', linewidth = 1.5)
+    mn = np.mean(slam_ar, axis = 0)
+    plt.errorbar(np.arange(N) + 1, mn, np.vstack([mn - slam_ar[p0025, :], slam_ar[p0975, :] - mn]), fmt = 'bo-')
+
+    mn = np.mean(slam_w1, axis = 0)
+    plt.errorbar(np.arange(N) + 1 + 0.1, mn, np.vstack([mn - slam_w1[p0025, :], slam_w1[p0975, :] - mn]), fmt = 'go-')
+    
+    mn = np.mean(slam_w2, axis = 0)
+    plt.errorbar(np.arange(N) + 1 + 0.2, mn, np.vstack([mn - slam_w2[p0025, :], slam_w2[p0975, :] - mn]), fmt = 'ko-')
+
+    mn = np.mean(slam_f, axis = 0)
+    plt.errorbar(np.arange(N) + 1 + 0.3, mn, np.vstack([mn - slam_f[p0025, :], slam_f[p0975, :] - mn]), fmt = 'mo-')
+    
+    plt.show()
+
     
 if __name__ == '__main__':
     
-    render_synth_model_component_eigvals()
+    render_slp_nh_component_eigvals()
+#    render_synth_model_component_eigvals()
 #    render_model_order_synth_model()
 
