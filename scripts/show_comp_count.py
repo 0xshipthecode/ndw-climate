40f772ac7896bc2b6340a9ac9a256c9974bfbcbd
Martin Vejmelka <vejmelkam@gmail.com>
2013-07-18 14:04:52 +0200

    added Bonferroni-Sidak single-level multiple comparison correction to multi_stats.py

--- scripts/show_comp_count.py
+++ scripts/show_comp_count.py
@@ -24,5 +24,6 @@ if __name__ == '__main__':
     p_vals = multi_stats.compute_eigvals_pvalues(dlam, slam)
     Nsurr = slam.shape[0]
     print("Bonferroni (single-level): %d components." % np.sum(multi_stats.bonferroni_test(p_vals, 0.05, Nsurr, Nhyp)))
+    print("Sidak (single-level): %d components." % np.sum(multi_stats.sidak_test(p_vals, 0.05, Nsurr, Nhyp)))
     print("Bonferroni-Holm (stepdown): %d components." % np.sum(multi_stats.holm_test(p_vals, 0.05, Nsurr, Nhyp)))
     print("False Discovery Rate: %d components." % np.sum(multi_stats.fdr_test(p_vals, 0.05, Nsurr, Nhyp)))
--- src/multi_stats.py
+++ src/multi_stats.py
@@ -42,6 +42,31 @@ def bonferroni_test(p_vals, sig_level, Nsurr, Nhyp = None):
     return p_vals < bonf_level
 
 
+def sidak_test(p_vals, sig_level, Nsurr, Nhyp = None):
+    """
+    Run a Bonferroni-Sidak multiple testing procedure on p-values <p_vals> with significance
+    level <sig_level> given that <Nsurr> surrogates were used to compute the p-values.
+    Optionally the number of simultaneously tested hypotheses can be set with <Nhyp>,
+    if left at None, len(p_vals) will be used.
+    """
+    # number of hypothesis may be set externally for robustness testing purposes
+    # i.e. does the number of components depende on the number of hypotheses?
+    if Nhyp is None:
+        Nhyp = len(p_vals)
+
+    # if the number of hypotheses is higher than the Nhyp (can happen for robustness testing)
+    # remove the tail elements of p_vals until p_vals has the length Nhyp
+    if Nhyp < len(p_vals):
+        p_vals = p_vals[:Nhyp]
+
+    sidak_level = 1.0 - (1.0 - sig_level) ** (1.0 / Nhyp)
+    
+    if sidak_level < 1.0 / Nsurr:
+        raise "Will not run Bonferroni-Sidak test, not enough surrogates available for the test!"
+    
+    return p_vals < sidak_level
+
+
 def fdr_test(p_vals, sig_level, Nsurr, Nhyp = None):
     """
     Run an FDR multiple testing procedure on p-values <p_vals> with significance
