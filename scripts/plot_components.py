57af79af423e0ab17aa9a2085767413ac42801bb
Martin Vejmelka <vejmelkam@gmail.com>
2013-07-29 13:56:13 +0200

    added new script plot estimated components using robinson projection, uses code of DH

---
+++ scripts/plot_components.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import geo_rendering as gr
+import numpy as np
+import sys
+import cPickle
+import os
+import matplotlib.pyplot as plt
+
+
+def tofield(d, lats, lons):
+    return d.reshape([len(lats), len(lons)])
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print("Usage: plot_components.py <component_file> <output_directory> <prefix>")
+        sys.exit(1)
+
+    # read in args
+    comp_file = sys.argv[1]
+    output_dir = sys.argv[2]
+    prefix = sys.argv[3]
+
+    with open(comp_file, 'r') as f:
+        data = cPickle.load(f)
+
+    # create output directory if non-existent
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+
+    # load results & their locations
+    lats, lons = data['lats'], data['lons']
+    cmps = data['Ur']
+    ts = np.asarray(data['ts'])
+    expvar = data['expvar']
+    regexpvar = data['reg_expvar']
+    total_var = data['total_var']
+    s2 = data['s2']
+
+    # mark binary components using a simplified method (not clustering compatible)
+    bin_cmps = cmps.argmax(1)
+
+    # this will plot the components in descending order of variance
+    # (the result file is already sorted this way)
+    centers = []
+    for i in range(cmps.shape[1]):
+        c_i = tofield(cmps[:,i], lats, lons)
+        flat_index = np.argmax(c_i)
+        clat, clon = np.unravel_index(flat_index, c_i.shape)
+        center_i = (lons[clon], lats[clat])
+        centers.append(center_i)
+        gr.plot_component_robinson(c_i, ts[i,:], lats, lons, center_i, regexpvar[i] / total_var,
+                                   filename = os.path.join(output_dir, '%s_comp_%d.png' % (prefix, i+1)))
+        plt.close("all")
+
+    # this will plot the clusters on one image
+    filename = os.path.join(output_dir, '%s_components.png' % prefix)
+    gr.plot_clusters_robinson(tofield(bin_cmps, lats, lons), lats, lons, centers = centers,
+                              filename = filename)
